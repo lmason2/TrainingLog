5989576cd8ceffdbab6e6f74ecc12335e63f0328
[Luke Mason] <lmason2@zagmail.gonzaga.edu>
2020-12-01 15:55:03 -0700

    Initial Commit

---
+++ ASUTrainingLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ASUTrainingLog
+//
+//  Created by Luke Mason on 12/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
